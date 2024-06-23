68b0509ac6e053426e15a89eda1361100ca91428
Clifford William <ccliffordwilliam@gmail.com>
2024-06-23 23:55:56 +0700

    style(node): timer better formatting
    for better maintainability

--- src/nodes/timer.py
+++ src/nodes/timer.py
@@ -1,4 +1,3 @@
-# from constants import
 from typing import Callable
 from typing import List
 
@@ -8,28 +7,50 @@ from typeguard import typechecked
 @typechecked
 class Timer:
     """
+    A countup timer.
+    Counts up and fire END event on passing duration.
+
     Set duration in float.
-    Call the draw and update method to start counting.
-    Call the reset before counting again.
+    Call the update method to start counting up.
+    Remember to call reset() before counting up again.
+
+    TODO: Timers are affected by Game.time_scale.
+    TODO: A higher scale means quicker timeouts, and vice versa.
     """
 
+    # Events.
+
+    # Fired when count reaches duration.
     END: int = 0
 
     def __init__(self, duration: float):
-        self.timer: int = 0
+        # Holds my counting.
+        self.count: int = 0
 
+        # Determine how long I count.
         self.duration: float = duration
 
+        # END event subscribers list.
         self.listener_end: List[Callable] = []
 
+        # True when I have counted past duration.
         self.is_done: bool = False
 
     def add_event_listener(self, value: Callable, event: int) -> None:
+        """
+        Use this to subscribe to my events.
+        """
+
         if event == self.END:
             self.listener_end.append(value)
 
     def reset(self) -> None:
-        self.timer = 0
+        """
+        Resets my count.
+        Is done False.
+        """
+
+        self.count = 0
 
         self.is_done = False
 
@@ -37,13 +58,19 @@ class Timer:
         """
         Update my counting until duration.
         """
+
+        # No need to count when is done true.
         if self.is_done:
             return
 
-        self.timer += dt
+        # Count.
+        self.count += dt
 
-        if self.timer > self.duration:
+        # Counted past duration?
+        if self.count > self.duration:
+            # Is done true.
             self.is_done = True
 
+            # Fire END event.
             for callback in self.listener_end:
                 callback()
